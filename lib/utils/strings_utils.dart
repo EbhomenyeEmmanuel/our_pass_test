eccb7340a9c2540ddf1009a432ea1d2cdc657880
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2022-07-19 11:22:15 +0100

    Created a String utility class

---
+++ lib/utils/strings_utils.dart
@@ -0,0 +1,9 @@
+class StringsUtils {
+  static const String fieldReq = 'This field is required';
+  static const String numberIsInvalid = 'Card is invalid';
+  static const String login = 'Login';
+  static const String signup = 'Sign Up';
+  static const String emailIsInvalid = 'Email is invalid';
+  static const String passwordIsInvalid = 'Password cannot be less than 6';
+  static const String usernameIsInvalid = 'Username cannot be less than 6';
+}
