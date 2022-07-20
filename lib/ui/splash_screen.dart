e01b484e061a1471c9c0d500eb7bdfbc26cd489d
Emmanuel.Ebhomenye <eebhomenye@yahoo.com>
2022-07-20 18:10:52 +0100

    Added screens to be navigated to

--- lib/ui/splash_screen.dart
+++ lib/ui/splash_screen.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:our_pass_test/provider/ui_manager.dart';
+import 'package:our_pass_test/ui/home/home.dart';
+import 'package:our_pass_test/ui/login/login.dart';
 import 'package:our_pass_test/ui/signup/sign_up.dart';
+import 'package:provider/provider.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
+  static List<Widget> pages = <Widget>[
+    const SignUpScreen(),
+    const LoginScreen(),
+    const Home(),
+    Container(
+      color: Colors.white,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
+    final uiManager = Provider.of<UiManager>(context);
     return Scaffold(
       body: FutureBuilder(
           future: Future.delayed(const Duration(seconds: 3)),
           builder: (context, timer) =>
               timer.connectionState == ConnectionState.done
-                  ? const SignUpScreen()
+                  ? pages[uiManager.currentScreen]
                   : const Center(child: CircularProgressIndicator())),
     );
   }
