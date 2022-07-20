6aa33eb66e38b3a226b3993c9e7dde79c79c27a3
Emmanuel.Ebhomenye <eebhomenye@yahoo.com>
2022-07-20 17:55:07 +0100

    Initialized dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:our_pass_test/injector.dart';
 import 'package:our_pass_test/provider/SignUpProvider.dart';
 import 'package:our_pass_test/provider/login_provider.dart';
+import 'package:our_pass_test/provider/ui_manager.dart';
 import 'package:our_pass_test/ui/splash_screen.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(create: (context) => LoginProvider()),
-    ChangeNotifierProvider(create: (context) => SignUpProvider())
-  ], child: const MyApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await initLocator();
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -16,12 +20,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<UiManager>(create: (context) => locator()),
+        ChangeNotifierProxyProvider<UiManager, SignUpProvider>(
+          create: (context) => locator(),
+          update: (BuildContext context, uiManager,
+                  SignUpProvider? signUpProvider) =>
+              signUpProvider!..update(uiManager),
+        ),
+        ChangeNotifierProxyProvider<UiManager, LoginProvider>(
+          create: (context) => locator(),
+          update:
+              (BuildContext context, uiManager, LoginProvider? loginProvider) =>
+                  loginProvider!..update(uiManager),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const SplashScreen(),
       ),
-      home: const SplashScreen(),
     );
   }
 }
