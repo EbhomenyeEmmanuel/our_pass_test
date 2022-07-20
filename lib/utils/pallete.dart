25933b421bba4211a7f59d22e0f6debd3c20aab0
Emmanuel.Ebhomenye <eebhomenye@yahoo.com>
2022-07-20 18:00:17 +0100

    Added a palette class

---
+++ lib/utils/pallete.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class Palette {
+  static const mainColor = const Color(0xFF2B872B);
+  static const headerColor = const Color(0xFF124B13);
+  static const mainTextColor = const Color(0xFF182A2C);
+  static const lightGreenColor = const Color(0xFFEAF3EA);
+  static const dividerColor = const Color(0xFFC4C4C4);
+}
